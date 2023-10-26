85e846986c050cef1e87cbb1a0cdb728c91acd19
Md Haider Hossain <mdhaiderhossain10@gmail.com>
2023-10-26 16:42:36 +0600

    hero section done

---
+++ src/components/pages/home/SeatInformation.jsx
@@ -0,0 +1,13 @@
+export default function SeatInformation() {
+    return (
+        <section>
+            <div className="container">
+                <div className="lg:w-10/12 mx-auto">
+                    <div className="bg-white py-[54px] px-[64px] rounded-[20px]">
+                        asdasd
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/pages/home/Hero";
+import SeatInformation from "@/components/pages/home/SeatInformation";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <SeatInformation />
         </>
     );
 }
